40b9188c568c9d9097faba86f28c05e14f2f1ba5
AhmedRaafat2016 <ahmedraah2013@gmail.com>
2018-05-14 13:00:43 +0200

    some init msg

---
+++ app/src/main/java/com/example/ahmad/chatapplication/MainActivity.java
@@ -0,0 +1,181 @@
+package com.example.ahmad.chatapplication;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import android.text.format.DateFormat;
+
+import java.io.ByteArrayOutputStream;
+
+import hani.momanii.supernova_emoji_library.Actions.EmojIconActions;
+import hani.momanii.supernova_emoji_library.Helper.EmojiconEditText;
+import hani.momanii.supernova_emoji_library.Helper.EmojiconTextView;
+
+public class MainActivity extends AppCompatActivity {
+    public ImageView imgGallery;
+    private static int SIGN_IN_REQUEST_CODE = 1;
+    private FirebaseListAdapter<ChatMessage> adapter;
+    RelativeLayout activity_main;
+
+    //Add Emojicon
+    EmojiconEditText emojiconEditText;
+    ImageView emojiButton,submitButton;
+    EmojIconActions emojIconActions;
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_sign_out)
+        {
+            AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    Snackbar.make(activity_main,"You have been signed out.", Snackbar.LENGTH_SHORT).show();
+                    finish();
+                }
+            });
+        }else if (item.getItemId()==R.id.menu_send_image){
+            /*Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.com_facebook_profile_picture_blank_portrait);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] byteArray = stream.toByteArray();*/
+            Intent myIntent = new Intent(Intent.ACTION_PICK);
+            //myIntent.putExtra("picture", byteArray);
+            myIntent.setType("image/*");
+            startActivityForResult(myIntent, 100);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SIGN_IN_REQUEST_CODE)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                Snackbar.make(activity_main,"Successfully signed in.Welcome!", Snackbar.LENGTH_SHORT).show();
+                displayChatMessage();
+            }}
+        else if(requestCode ==100)
+            {
+                //gallery
+
+                if(resultCode==RESULT_OK)
+                {
+                    imgGallery = (ImageView) findViewById(R.id.imgGallery);
+
+                    Uri imageUri = data.getData();
+
+                    imgGallery.setImageURI(imageUri);
+
+
+
+                }
+
+            }
+            else{
+                Snackbar.make(activity_main,"We couldn't sign you in.Please try again later", Snackbar.LENGTH_SHORT).show();
+                finish();
+            }
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        activity_main = (RelativeLayout)findViewById(R.id.activity_main);
+
+        //Add Emoji
+        emojiButton = (ImageView)findViewById(R.id.emoji_button);
+        submitButton = (ImageView)findViewById(R.id.submit_button);
+        emojiconEditText = (EmojiconEditText)findViewById(R.id.emojicon_edit_text);
+        emojIconActions = new EmojIconActions(getApplicationContext(),activity_main, emojiconEditText, emojiButton);
+        emojIconActions.ShowEmojIcon();
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase.getInstance().getReference().push().setValue(new ChatMessage(emojiconEditText.getText().toString(),
+                        FirebaseAuth.getInstance().getCurrentUser().getEmail()));
+                emojiconEditText.setText("");
+                emojiconEditText.requestFocus();
+
+            }
+        });
+
+        //Check if not sign-in then navigate Signin page
+        if(FirebaseAuth.getInstance().getCurrentUser() == null)
+        {
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(),SIGN_IN_REQUEST_CODE);
+        }
+        else
+        {
+            Snackbar.make(activity_main,"Welcome "+FirebaseAuth.getInstance().getCurrentUser().getEmail(),Snackbar.LENGTH_SHORT).show();
+            //Load content
+            displayChatMessage();
+        }
+
+
+    }
+
+
+
+    private void displayChatMessage() {
+
+        ListView listOfMessage = (ListView)findViewById(R.id.list_of_message);
+        adapter = new FirebaseListAdapter<ChatMessage>(this,ChatMessage.class,R.layout.list_item,FirebaseDatabase.getInstance().getReference())
+        {
+            @Override
+            protected void populateView(View v, ChatMessage model, int position) {
+
+                //Get references to the views of list_item.xml
+                TextView messageText,  messageUser, messageTime;
+                messageUser = (TextView) v.findViewById(R.id.message_user);
+                messageTime = (TextView) v.findViewById(R.id.message_time);
+                messageText = (TextView) v.findViewById(R.id.message_text);
+               messageText.setText(model.getMessageText());
+               /* Bundle extras = getIntent().getExtras();
+                byte[] byteArray = extras.getByteArray("picture");
+                Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+                imgGallery = (ImageView) findViewById(R.id.imgGallery);
+                imgGallery.setImageBitmap(bmp);*/
+                messageUser.setText(model.getMessageUser());
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)", model.getMessageTime()));
+
+            }
+        };
+        listOfMessage.setAdapter(adapter);
+    }
+}
